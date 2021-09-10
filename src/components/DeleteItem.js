3007895c80deb2f3e2b33263150b8a4340ad704e
Sutirtha Dey <sutirtha.dey.17@mountblue.tech>
2021-09-10 18:00:51 +0530

    added the DeleteItem function from the parent component

--- src/components/DeleteItem.js
+++ src/components/DeleteItem.js
@@ -1,10 +1,17 @@
 import { FaTrashAlt } from 'react-icons/fa';
 
-const DeleteItem = ({ item, deleteElements }) => {
+const DeleteItem = ({ task, tasklist, setTasklist }) => {
+  const deleteElements = (id) => {
+    const updatedTasklist = tasklist.filter((task) => {
+      return task.id !== id;
+    });
+
+    setTasklist(updatedTasklist);
+  };
   return (
     <button
       title='Delete Item'
-      onClick={() => deleteElements(item.id)}
+      onClick={() => deleteElements(task.id)}
       className='btn btn-block'
     >
       <FaTrashAlt className='icon-delete' />
