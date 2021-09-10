9337cd10207186e961e34d0e87c8b6db2df55773
Sutirtha Dey <sutirtha.dey.17@mountblue.tech>
2021-09-10 18:03:01 +0530

    added editItem function from the parent component

--- src/components/EditItem.js
+++ src/components/EditItem.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import { FaEdit } from 'react-icons/fa';
 
-const EditItem = ({ item, editItem }) => {
+const EditItem = ({
+  task,
+  tasklist,
+  setActivity,
+  setTimeOfActivity,
+  setRemainder,
+  setToggleAddAndEdit,
+  setUpdateItem,
+}) => {
+  const editItem = (id) => {
+    let taskToBeEdited = tasklist.find((task) => {
+      return task.id === id;
+    });
+    setToggleAddAndEdit(false);
+
+    setActivity(taskToBeEdited.name);
+
+    setTimeOfActivity(taskToBeEdited.time);
+
+    setRemainder(taskToBeEdited.remainder);
+
+    setUpdateItem(id);
+  };
   return (
     <button
       title='Edit Item'
-      onClick={() => editItem(item.id)}
+      onClick={() => editItem(task.id)}
       className='btn btn-block'
     >
       <FaEdit className='icon-update' />
